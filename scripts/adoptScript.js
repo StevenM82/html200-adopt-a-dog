4a934ba459c0fb85ed82db44786600a22e1480d7
Gitting better <StevenM82@users.noreply.github.com>
2021-06-20 15:27:01 -0700

    figured out how to convert the adoption price to a number that can be added in js

--- scripts/adoptScript.js
+++ scripts/adoptScript.js
@@ -1,34 +1,52 @@
 let name;
 let breed = "Breed: Dog";
 let adoptFee;
-let parent;
+let parent = document.getElementsByTagName("article"); //shows how many elements of tag <article>
+
 
 function infoAboutDogs(thisDog) {
-    parent = document.getElementsByTagName("article"); //shows how many elements of tag <article>
-    
-    name = parent[2].children[1].textContent; //gives me the content of <h3> of the <article> element!!
-    parented2 = parent[1].children[2].textContent;
+    name = parent[2].children[1].textContent; //gives the content of <h3> of the <article> element
+    information = parent[1].children[2].textContent; //gives the content of the first <p> element
 
     if (name == "Poppy") {
-        alert(`${name} \n${breed} \n${parented2}`); //alerts name, breed and cost to adopt
+        alert(`${name} \n${breed} \n${information}`); //alerts name, breed and cost to adopt in a column format
     } 
+
     console.log(name);
     console.log(parent);
 }
+//This requires me to pass static information to the function in the script...
+
 
 /* This would get the information from any <article>
 article -> 
     class="article-" -> (articles 0 - 12)
         children -> 
-            children element[1] <h3> -> .innerText  "name"
-            children element[2] <p> -> .innerText   "cost to adopt: 123.45"
-                .innerText (convert the text from children element[2] into an array) .split()
+            children element[1] <h3> -> .textContent  "name"
+            children element[2] <p> -> .textContent   "cost to adopt: 123.45"
+                .textContent (convert the text from children element[2] into an array) .split()
                 take the last item from that array and convert it to a number. Number(adoptFee)
 
 */
 
+let information = parent[1].children[2].textContent;
+moreInfo = information.replace('$', '');
+console.log(moreInfo);
+
+newInfo = moreInfo.split(' ');
+console.log(newInfo);
+console.log(newInfo[3]);
+
+moreNewInfo = Number(newInfo[3]);
+console.log(moreNewInfo);
+console.log(moreNewInfo + moreNewInfo);
+
+
+
+
 function runningTotal(total) {
-    let currentAmount = 0; 
-    let input = 0;
+    
 }
 
+let currentAmount = 0; 
+    let input = 0;
