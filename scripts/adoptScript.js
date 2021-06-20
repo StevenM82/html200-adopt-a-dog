87d2a882e4bee6ed5ab7cfbd3c3ee453243ea597
Gitting better <StevenM82@users.noreply.github.com>
2021-06-20 12:10:44 -0700

    starting to figure out how to reference specific parts of each article

--- scripts/adoptScript.js
+++ scripts/adoptScript.js
@@ -6,23 +6,26 @@ let blah;
 function infoAboutDogs(thisDog) {
     parent = document.getElementsByTagName("article"); //shows how many elements of tag <article>
         //parent = document.getElementsByTagName("article")[1]; references the second artical information
+    
+    parent[1].style.backgroundColor = "yellow";
+    parent[2].style.fontWeight = "bold";
+    parented = parent[1].children[1].textContent; //gives me the content of <h3> of the second <article> element!!
     let result = parent.textContent; //shows all of the text for a given article. have to pass a specific article to it
-    blah = document.getElementsByClassName("article-1");
-    //alert(`${name} ${breed} ${adoptFee}`);
-    //alert(result);
+
+    console.log(parented);
+    alert(parented);
     console.log(parent);
     //console.log(blah);
-    console.log(result);
 }
 
 /* This would get the information from any <article>
 article -> 
-    class="article-" -> (articles 1 - 8)
+    class="article-" -> (articles 0 - 12)
         children -> 
             children element[1] <h3> -> .innerText  "name"
             children element[2] <p> -> .innerText   "cost to adopt: 123.45"
                 .innerText (convert the text from children element[2] into an array) .split()
-                (take the last item from that array)
+                take the last item from that array and convert it to a number. Number(adoptFee)
 
 */
 
