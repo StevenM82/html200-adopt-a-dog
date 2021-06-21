dc54c303ecc8795ef8d993c0954f30eae69d37e8
Gitting better <StevenM82@users.noreply.github.com>
2021-06-20 19:46:27 -0700

    getting ready to add id's to dog names

--- scripts/adoptScript.js
+++ scripts/adoptScript.js
@@ -1,52 +1,33 @@
-let name;
-let breed = "Breed: Dog";
-let adoptFee;
-let parent = document.getElementsByTagName("article"); //shows how many elements of tag <article>
-
-
 function infoAboutDogs(thisDog) {
-    name = parent[2].children[1].textContent; //gives the content of <h3> of the <article> element
-    information = parent[1].children[2].textContent; //gives the content of the first <p> element
+    let breed = "Breed: Dog";
+    let parent = document.getElementsByTagName("article"); //shows how many elements of tag <article>
+    let name = parent[2].children[1].textContent; //.children[1] gives "name"
+    let information = parent[1].children[2].textContent; //.children[2] gives "cost to adopt: $123.45"
+    let adoptInfo = information.replace('$', '');
+    let newInfo = adoptInfo.split(' ');
+    let adoptFee = Number(newInfo[3]); //presently set to $123.45 per dog
 
+    console.log(parent);
+    
     if (name == "Poppy") {
         alert(`${name} \n${breed} \n${information}`); //alerts name, breed and cost to adopt in a column format
-    } 
+    } else {
+        alert("nothing");
+    }
 
-    console.log(name);
-    console.log(parent);
 }
 //This requires me to pass static information to the function in the script...
 
 
-/* This would get the information from any <article>
-article -> 
-    class="article-" -> (articles 0 - 12)
-        children -> 
-            children element[1] <h3> -> .textContent  "name"
-            children element[2] <p> -> .textContent   "cost to adopt: 123.45"
-                .textContent (convert the text from children element[2] into an array) .split()
-                take the last item from that array and convert it to a number. Number(adoptFee)
-
-*/
-
-let information = parent[1].children[2].textContent;
-moreInfo = information.replace('$', '');
-console.log(moreInfo);
-
-newInfo = moreInfo.split(' ');
-console.log(newInfo);
-console.log(newInfo[3]);
-
-moreNewInfo = Number(newInfo[3]);
-console.log(moreNewInfo);
-console.log(moreNewInfo + moreNewInfo);
-
-
-
-
 function runningTotal(total) {
+    dogAlreadySelected = false;
+    dogs = document.getElementsByClassName("adopt");
+    console.log(dogs);
+
+    let previousTotal = 0;
+    let currentTotal = 0; 
+    //alert(currentTotal);
+
+
     
 }
-
-let currentAmount = 0; 
-    let input = 0;
